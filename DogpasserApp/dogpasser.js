632872382197e2fc0460d022bd39be67bff34797
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-02-26 16:04:30 +0100

    dogpasser brukerkort oppdatering

--- DogpasserApp/dogpasser.js
+++ DogpasserApp/dogpasser.js
@@ -3,17 +3,28 @@ const showCardsBtn = document.querySelector("#show-cards-btn").addEventListener(
 let allUsers = fetchUsers();
 
 async function fetchUsers() {
-	const request = await fetch("https://randomuser.me/api/?results=500"); //fetcher 500 users
-	let result = await request.json();
-	return result;
-}
+	const request = await fetch("https://randomuser.me/api/?results=500&inc=name,location,picture");
+	let { results } = await request.json();
 
-async function fetchRandomDogImg() {
-	const request = await fetch("https://dog.ceo/api/breeds/image/random");
-	let result = await request.json();
-	return result;
+	return results.map(({ name, location, picture }) => ({
+		name: name.first,
+		location: { city: location.city, state: location.state },
+		thumbnail: picture.thumbnail,
+	}));
 }
 
 function showCards() {
 	console.log("Inne i showCards!");
+}
+
+//fetchRandomDogImg & vis i HTML
+/*
+legg til:
+getelementybyid random-dog-img = randomdogimg
+insert img into html
+*/
+async function fetchRandomDogImg() {
+	const request = await fetch("https://dog.ceo/api/breeds/image/random");
+	let result = await request.json();
+	return result;
 }
