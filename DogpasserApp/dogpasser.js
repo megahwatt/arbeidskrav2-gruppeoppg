4c95fd2b8f256db04622d0ce5dfadedc540dba29
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-02-26 18:20:58 +0100

    dogpasser kortet på startsiden viser nå all påkrevd informasjon

--- DogpasserApp/dogpasser.js
+++ DogpasserApp/dogpasser.js
@@ -1,6 +1,11 @@
+// showCardsBtn --- Vis 10 nye kort --- ikke ferdig
 const showCardsBtn = document.querySelector("#show-cards-btn").addEventListener("click", showCards);
 
-// fetchAndDisplayRandomUser
+function showCards() {
+	console.log("Inne i showCards!");
+}
+
+// fetchAndDisplayRandomUser --- henter inn brukere fra API og viser dem i nederste delen av kortet
 async function fetchAndDisplayRandomUser() {
 	try {
 		const request = await fetch("https://randomuser.me/api/?results=500&nat=us&inc=name,location,picture");
@@ -14,27 +19,27 @@ async function fetchAndDisplayRandomUser() {
 
 		const chosenUser = users[Math.floor(Math.random() * users.length)];
 		const randomUser = document.querySelector(".random-user");
-		randomUser.innerHTML = `<img src="${chosenUser.thumbnail}">
-    <p>Navn: ${chosenUser.name}</p>
-    <p>Bosted: ${chosenUser.location.city}, ${chosenUser.location.state}</p>`;
+		randomUser.innerHTML = `
+        <img src="${chosenUser.thumbnail}">
+        <p>Navn: ${chosenUser.name}</p>
+        <p>Bosted: ${chosenUser.location.city}, ${chosenUser.location.state}</p>`;
 	} catch (error) {
 		console.log("404", error);
 	}
 }
+
 fetchAndDisplayRandomUser();
 
-function showCards() {
-	console.log("Inne i showCards!");
+// fetchRandomDogImg --- henter inn et tilfeldig bilde fra API og viser den i øverste delen av kortet
+async function fetchRandomDogImg() {
+	try {
+		const request = await fetch("https://dog.ceo/api/breeds/image/random");
+		const dogImg = await request.json();
+
+		document.getElementById("random-dog-img").src = dogImg.message;
+	} catch (error) {
+		console.log("404", error);
+	}
 }
 
-//fetchRandomDogImg & vis i HTML
-/*
-legg til:
-getelementybyid random-dog-img = randomdogimg
-insert img into html
-*/
-async function fetchRandomDogImg() {
-	const request = await fetch("https://dog.ceo/api/breeds/image/random");
-	let result = await request.json();
-	return result;
-}
+fetchRandomDogImg();
