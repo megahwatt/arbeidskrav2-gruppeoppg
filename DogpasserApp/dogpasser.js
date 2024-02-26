f125f6c05f05d913d055aa8a78b185c0d32783a0
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-02-26 17:23:21 +0100

    dogpasser kortet viser brukerprofil og knapper for slett og chat

--- DogpasserApp/dogpasser.js
+++ DogpasserApp/dogpasser.js
@@ -1,17 +1,27 @@
 const showCardsBtn = document.querySelector("#show-cards-btn").addEventListener("click", showCards);
 
-let allUsers = fetchUsers();
+// fetchAndDisplayRandomUser
+async function fetchAndDisplayRandomUser() {
+	try {
+		const request = await fetch("https://randomuser.me/api/?results=500&nat=us&inc=name,location,picture");
+		const { results } = await request.json();
 
-async function fetchUsers() {
-	const request = await fetch("https://randomuser.me/api/?results=500&inc=name,location,picture");
-	let { results } = await request.json();
+		const users = results.map(({ name, location, picture }) => ({
+			name: name.first,
+			location: { city: location.city, state: location.state },
+			thumbnail: picture.thumbnail,
+		}));
 
-	return results.map(({ name, location, picture }) => ({
-		name: name.first,
-		location: { city: location.city, state: location.state },
-		thumbnail: picture.thumbnail,
-	}));
+		const chosenUser = users[Math.floor(Math.random() * users.length)];
+		const randomUser = document.querySelector(".random-user");
+		randomUser.innerHTML = `<img src="${chosenUser.thumbnail}">
+    <p>Navn: ${chosenUser.name}</p>
+    <p>Bosted: ${chosenUser.location.city}, ${chosenUser.location.state}</p>`;
+	} catch (error) {
+		console.log("404", error);
+	}
 }
+fetchAndDisplayRandomUser();
 
 function showCards() {
 	console.log("Inne i showCards!");
