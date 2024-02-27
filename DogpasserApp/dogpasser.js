b6bf028228abb1b0172a05498205cebc25b6cf5a
camillachr <chri.camilla@gmail.com>
2024-02-27 14:14:42 +0100

    push

--- DogpasserApp/dogpasser.js
+++ DogpasserApp/dogpasser.js
@@ -1,21 +1,59 @@
-const showCardsBtn = document
-  .querySelector("#show-cards-btn")
-  .addEventListener("click", showCards);
+//fetch EN random user med bilde, navn og lokasjon
 
-let allUsers = fetchUsers();
+async function fetchRandomUser() {
+  try {
+    const request = await fetch(
+      "https://randomuser.me/api/?inc=picture,name,location"
+    );
+    const response = await request.json();
+    let randomUser = response.results;
 
-async function fetchUsers() {
-  const request = await fetch("https://randomuser.me/api/?results=500"); //fetcher 500 users
-  let result = await request.json();
-  return result;
+    return randomUser;
+  } catch (error) {
+    console.error("Kunne ikke hente users", error);
+  }
 }
+
+//fetch ETT tilfeldig hundebilde, men kun ut ifra 5 valgte raser
+
+const dogBreeds = ["havanese", "dingo", "pitbull", "akita", "eskimo"];
 
 async function fetchRandomDogImg() {
-  const request = await fetch("https://dog.ceo/api/breeds/image/random");
-  let result = await request.json();
-  return result;
+  try {
+    let randomDogBreed =
+      dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+    const request = await fetch(
+      `https://dog.ceo/api/breed/${randomDogBreed}/images/random`
+    );
+    const response = await request.json();
+    let randomDogImg = response.message;
+
+    return randomDogImg;
+  } catch (error) {
+    console.error("Kunne ikke hente hundebilde", error);
+  }
 }
 
-function showCards() {
-  console.log("Inne i showCards!");
+// Globale variabler
+const cardContainer = document.querySelector("#card-container");
+const showCardsBtn = document
+  .querySelector("#show-cards-btn")
+  .addEventListener("click", createAndShowCards());
+
+//Lage og vise kort på siden
+async function createAndShowCards() {
+  cardContainer.innerHTML = "";
+
+  for (let i = 0; i < 10; i++) {
+    let dogImgUrl = await fetchRandomDogImg();
+    let user = await fetchRandomUser();
+
+    let profileCard = document.createElement("div");
+    profileCard.innerHTML = `<img src="${dogImgUrl}" width="200px"/>
+    <img src="${user[0].picture.large}" width="100px"/>
+      <p>Navn: ${(user[0].name.first, user[0].name.last)}</p>
+      <p>Bosted: ${user[0].location.city}</p>`;
+    profileCard.classList.add("profile-card");
+    cardContainer.append(profileCard);
+  }
 }
