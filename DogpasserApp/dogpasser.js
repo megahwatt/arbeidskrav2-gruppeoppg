0cca1b548f64e18d3960a740d7cc50c04dae3e79
camillachr <chri.camilla@gmail.com>
2024-02-28 15:57:52 +0100

    Omskriving av både js, html og css. Koden funker men stylingen (min) ser forferdelig ut:)

--- DogpasserApp/dogpasser.js
+++ DogpasserApp/dogpasser.js
@@ -1,19 +1,25 @@
-//fetch EN random user med bilde, navn og lokasjon. Vurdere om vi heller bør hente 10 stk samtidig her?
+// Globale variabler
+const cardContainer = document.querySelector(".card-container");
+const showCardsBtn = document.querySelector("#show-cards-btn");
+showCardsBtn.onclick = createAndShowCards;
 
+//fetch en random user med bilde, navn og lokasjon
 async function fetchRandomUser() {
   try {
     const request = await fetch(
       "https://randomuser.me/api/?nat=us&inc=picture,name,location"
     );
-    let { results } = await request.json();
-    return results;
+    const data = await request.json();
+    const user = data.results[0];
+    console.log("Inne i fetchRandomUser", user);
+
+    return user;
   } catch (error) {
     console.error("Kunne ikke hente users", error);
   }
 }
 
-//fetch ETT tilfeldig hundebilde, men kun ut ifra 5 valgte raser
-
+//fetch ett tilfeldig hundebilde, men kun ut ifra 5 valgte raser
 const dogBreeds = ["havanese", "dingo", "pitbull", "akita", "eskimo"];
 
 async function fetchRandomDogImg() {
@@ -32,26 +38,47 @@ async function fetchRandomDogImg() {
   }
 }
 
-// Globale variabler
-const cardContainer = document.querySelector("#card-container");
-
-//Lage og vise kort på siden. Her har jeg appendet alt inn i profile-card, så det må vi endre på slik at de appender til hver sin div, slik det er delt inn i HTML-koden.
+//Lage og vise kort på siden.
+//Denne funksjonen må deles opp i mindre ansvarsområder, men da må vi nok inn med parametere, hufff.
 async function createAndShowCards() {
   cardContainer.innerHTML = "";
 
   for (let i = 0; i < 10; i++) {
-    let dogImgUrl = await fetchRandomDogImg();
-    let user = await fetchRandomUser();
+    //henter inn user og dog
+    const dogImgUrl = await fetchRandomDogImg();
+    const user = await fetchRandomUser();
 
-    let profileCard = document.createElement("div");
-    profileCard.innerHTML = `<img src="${dogImgUrl}" width="200px"/>
-    <img src="${user[0].picture.large}" width="100px"/>
-      <p>Navn: ${user[0].name.first + user[0].name.last}</p>
-      <p>Bosted: ${user[0].location.city}</p>`;
+    //lager selve kortet
+    const profileCard = document.createElement("div");
+    const dogImgContainer = document.createElement("div");
+    const userContainer = document.createElement("div");
+    const userImg = document.createElement("div");
+    const userTxt = document.createElement("div");
+    const btnContainer = document.createElement("div");
+    const deleteBtn = document.createElement("button");
+    const chatBtn = document.createElement("button");
+
+    //legger til klasse på hvert element
     profileCard.classList.add("profile-card");
-    cardContainer.append(profileCard); //denne må endres til
+    dogImgContainer.classList.add("dog-img-container");
+    userContainer.classList.add("user-container");
+    userImg.classList.add("user-img");
+    userTxt.classList.add("user-txt");
+    btnContainer.classList.add("btn-container");
+    deleteBtn.classList.add("delete-btn");
+    chatBtn.classList.add("chat-btn");
+
+    //legger til innhold i elementene på kortet
+    dogImgContainer.innerHTML = `<img src="${dogImgUrl}" id="dog-img">`;
+    userImg.innerHTML = `<img src="${user.picture.large}" />`;
+    userTxt.innerHTML = `<p>Navn: ${user.name.first} ${user.name.last}</p>      <p>Bosted: ${user.location.city}</p>`;
+    deleteBtn.innerHTML = "Slett";
+    chatBtn.innerHTML = "Chat";
+
+    //appender alt
+    cardContainer.append(profileCard);
+    profileCard.append(dogImgContainer, userContainer, btnContainer);
+    userContainer.append(userImg, userTxt);
+    btnContainer.append(chatBtn, deleteBtn);
   }
 }
-const showCardsBtn = document
-  .querySelector("#show-cards-btn")
-  .addEventListener("click", createAndShowCards());
