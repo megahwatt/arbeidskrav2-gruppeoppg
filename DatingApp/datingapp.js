9f5aa906d91e5e793b7d493fa92d6db9211d04fc
lallod <lalo.peshawa@gmail.com>
2024-03-04 22:35:18 +0100

    adding css - js

---
+++ DatingApp/datingapp.js
@@ -0,0 +1,7 @@
+const scoreElement = document.getElementById('score');
+const nameElement = document.querySelector('.name');
+const locationElement = document.querySelector('.location');
+const profileImgElement = document.querySelector('.profile-card img');
+const notInterestedBtn = document.getElementById('not-interested');
+const interestedBtn = document.getElementById('interested');
+const messageElement = document.getElementById('message');
