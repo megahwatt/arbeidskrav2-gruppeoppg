7d441bd80bb9c945cbd5f855f4b1cfb6b7d8cd20
megahwatt <156299328+megahwatt@users.noreply.github.com>
2024-03-05 17:15:08 +0100

    oppdatere js

--- DogpasserApp/sletteknapp.js
+++ DogpasserApp/sletteknapp.js
@@ -2,52 +2,64 @@
 const cardContainer = document.querySelector(".card-container");
 
 const showCardsBtn = document.querySelector("#show-cards-btn");
-showCardsBtn.onclick = showCards;
+showCardsBtn.addEventListener("click", getNewCards);
 
-const deleteBtn = document.querySelector(".delete-btn");
+const breeds = ["labrador", "germanshepherd", "husky", "beagle", "akita"];
+
+let currentUsers = [];
 
 // fetchRandomUser -- henter inn 10 brukere
-async function fetchRandomUser() {
+async function fetchRandomUserWithDog() {
 	try {
-		const request = await fetch("https://randomuser.me/api/?results=10&nat=us&inc=picture,name,location");
+		const request = await fetch("https://randomuser.me/api/?results=1&nat=us&inc=name,location,picture");
 		const data = await request.json();
 		const user = data.results[0];
+		const randomDog = await fetchRandomDog();
 
-		return user;
+		const userWithDog = {
+			name: `${user.name.last}, ${user.name.first}`,
+			location: `${user.location.city}, ${user.location.state}`,
+			userImg: user.picture.large,
+			dogImg: randomDog.url,
+			dogBreed: randomDog.breed,
+		};
+
+		currentUsers.unshift(userWithDog);
 	} catch (error) {
-		console.error("Kunne ikke hente users", error);
+		console.error("Kunne ikke hente user og randomdog", error);
 	}
 }
 
 // fetchRandomDogImg -- henter inn bilder av hunder basert på et rase-filter
-async function fetchRandomDogImg() {
-	const dogBreeds = ["labrador", "germanshepherd", "husky", "beagle", "akita"];
+async function fetchRandomDog() {
 	try {
-		let randomDogBreed = dogBreeds[Math.floor(Math.random() * dogBreeds.length)];
+		const randomDogBreed = breeds[Math.floor(Math.random() * breeds.length)];
 		const request = await fetch(`https://dog.ceo/api/breed/${randomDogBreed}/images/random`);
 		const response = await request.json();
-		let randomDogImg = response.message;
+		const randomDog = { url: response.message, breed: randomDogBreed };
 
-		return randomDogImg;
+		return randomDog;
 	} catch (error) {
 		console.error("Kunne ikke hente hundebilde", error);
 	}
 }
 
-// showCards -- viser 10 nye kort hver gang man trykker på knappen, og legger til index-paramteret i kortene
-async function showCards() {
-	const cardContainer = document.querySelector("card-container");
-
-	cardContainer.innerHTML = "";
-
-	card.forEach((user, index) => {
-		const 
-	})
-	for (let i = 0; i < 10; i++) {
-		const card = await createCard(i);
-		cardContainer.appendChild(card);
-	}
+//cemptyCurrentUsers -- tømmer users-arrayet hver gang så det bare vises 10 av gangen
+function emptyCurrentUsers() {
+	currentUsers = [];
 }
+
+// showCards -- viser 10 nye kort hver gang man trykker på knappen, og legger til index-paramteret i kortene
+async function getNewCards() {
+	emptyCurrentUsers();
+	for (let i = 0; i < 10; i++) {
+		await fetchRandomUserWithDog();
+	}
+	createAndShowCards(currentUsers);
+}
+
+// viser kort når siden lastes
+getNewCards();
 
 //slettefunksjon
 function deleteCard(index) {
@@ -106,4 +118,18 @@ async function createCard(index) {
 	}
 }
 
-showCards();
+//filter
+const breedFilter = document.querySelector("#breed-filter");
+const filterBtn = document.querySelector("#filter-btn");
+filterBtn.addEventListener("click", filterByBreed);
+
+function filterByBreed() {
+	selectedBreed = breedFilter.value;
+
+	if (selectedBreed == "all") {
+		createAndShowCards(currentUsers);
+	} else {
+		filteredUsers = currentUsers.filter((user) => user.dogBreed == selectedBreed);
+		createAndShowCards(filteredUsers);
+	}
+}
